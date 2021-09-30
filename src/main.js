6dd5fc6bf7987d42e52b6306e1944a089eabe4da
OleksiyRiabiy <63644313+OleksiyRiabiy@users.noreply.github.com>
2021-09-30 11:59:20 +0300

    refaсtoring

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import App from "./App.vue";
+import "./assets/tailwind.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(ElementPlus);
+
+app.mount("#app");
